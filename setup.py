a315376c76ebb0e873ce021d908692274b8523dc
132nd-etcher <132nd-etcher@daribouca.net>
2017-02-10 19:47:17 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='SLTP',
+    version='0.0.1',
+    author='132nd-etcher',
+    url='https://github.com/132nd-etcher/sltp',
+    packages=['sltp', 'tests'],
+    requires=['mpmath']
+)
