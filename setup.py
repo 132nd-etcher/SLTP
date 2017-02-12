52b86a3b74bf8db8c5b9d9b3771ecd0f482123f1
132nd-etcher <132nd-etcher@daribouca.net>
2017-02-12 13:24:00 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='SLTP',
-    version='0.0.1',
+    version='0.0.2',
     author='132nd-etcher',
     url='https://github.com/132nd-etcher/sltp',
     packages=['sltp'],
