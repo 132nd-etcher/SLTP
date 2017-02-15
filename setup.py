096d81c24bb1361163563478f0e18367050f7036
132nd-etcher <132nd-etcher@daribouca.net>
2017-02-15 11:33:26 +0100

    auto-sorting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # noinspection SpellCheckingInspection
 setup(
     name='SLTP',
-    version='0.0.2',
+    version='0.0.3',
     author='132nd-etcher',
     url='https://github.com/132nd-etcher/sltp',
     packages=['sltp'],
