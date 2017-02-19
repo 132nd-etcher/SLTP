aed56768ae0a44474a04811d5c87d69d1012784d
132nd-etcher <132nd-etcher@daribouca.net>
2017-02-19 12:00:17 +0100

    0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # noinspection SpellCheckingInspection
 setup(
     name='SLTP',
-    version='0.0.4',
+    version='0.0.5',
     author='132nd-etcher',
     url='https://github.com/132nd-etcher/sltp',
     packages=['sltp'],
