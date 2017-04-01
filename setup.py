f07dfff4ed3bac583b9e19bdec6a914dd65a3825
132nd-etcher <132nd-etcher@daribouca.net>
2017-04-01 16:25:00 +0200

    0.0.10
    manage empty lua tables

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # noinspection SpellCheckingInspection
 setup(
     name='SLTP',
-    version='0.0.9',
+    version='0.0.10',
     author='132nd-etcher',
     url='https://github.com/132nd-etcher/sltp',
     packages=['sltp'],
